af54cea1325a678b064b270707012b4cb18d12e7
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-08-23 23:15:17 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -35,9 +35,17 @@ async def batch(client: Client, message: Message):
 
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
+    link = f"https://telegram.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    da_urlx = "https://da.gd/"
+    respx = f"https://tnlinks.in/api?api=1458ad61946fd6f5b8a93161c9cfd94733813566&url={link}&format=text"
+    fuksharex = requests.get(respx)
+    tsharex = fuksharex.text
+    csharex = tsharex
+    xshare_urlx = f"{da_urlx}shorten"
+    tgsharex = requests.get(xshare_urlx, params={"url": csharex})
+    telesharex = tgsharex.text.strip() 
+    await second_message.reply_text(f"START: https://t.me/c/1441020553/{f_msg_id}\nEND:https://t.me/c/1441020553/{s_msg_id}\n\n <b>Here is your link</b>\n\n{link}\n\n<b>TN LINK</b>\n\n`{telesharex}`", quote=True, reply_markup=reply_markup)
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
